7da8a5d0f02ef0497311554fa1ea63e21b29a93a
capt2fact-cyber <capt2fact@gmail.com>
2026-04-28 15:51:50 +0900

    feat: update dependencies and map configuration

--- the_truth_filtering_engine/lib/features/1-1_map/screens/map_screen.dart
+++ the_truth_filtering_engine/lib/features/1-1_map/screens/map_screen.dart
@@ -317,11 +317,7 @@ class _MapScreenState extends ConsumerState<MapScreen> {
           'lat': center.latitude.toString(),
           'lng': center.longitude.toString(),
           'radius': radiusMeters.toString(),
-<<<<<<< HEAD
           'display': '10',
-=======
-          'display': '20',
->>>>>>> origin/dev
         },
       );
 
