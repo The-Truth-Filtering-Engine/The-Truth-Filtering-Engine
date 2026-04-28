9791532b1117cff937b02e80ef694673c6d4e369
Choi Hyunseok <testwelltest01@gmail.com>
2026-04-28 17:29:23 +0900

    fix: 서버 ip를 localhost로 변경

--- the_truth_filtering_engine/lib/features/1-3_restaurant_detail/screens/restaurant_detail_screen.dart
+++ the_truth_filtering_engine/lib/features/1-3_restaurant_detail/screens/restaurant_detail_screen.dart
@@ -50,7 +50,7 @@ Future<List<BlogReview>> _fetchCachedReviews(
 Future<List<BlogReview>> _fetchFreshReviews(
     String name, AnalysisMode mode) async {
   final uri =
-      Uri.parse('http://172.30.1.42:8000/api/search').replace(queryParameters: {
+      Uri.parse('http://localhost:8000/api/search').replace(queryParameters: {
     'query': name,
     'mode': mode.name,
   });
@@ -160,8 +160,7 @@ class _RestaurantDetailScreenState
   @override
   Widget build(BuildContext context) {
     final bookmarkedRestaurants = ref.watch(bookmarkRestaurantsProvider);
-    final isBookmarked =
-        bookmarkedRestaurants.any((item) => item.id == _r.id);
+    final isBookmarked = bookmarkedRestaurants.any((item) => item.id == _r.id);
 
     return Scaffold(
       backgroundColor: AppColors.background,
