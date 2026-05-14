fff9899439c8f155091d1dbd8252b1a4c4681f2d
Choi Hyunseok <testwelltest01@gmail.com>
2026-05-14 16:42:59 +0900

    feat: 웹페이지 첫 화면 수정4

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -159,7 +159,7 @@ function App() {
   const map = useMap(handleMapSelectRestaurant, showToast)
 
   useEffect(() => {
-    const timer = window.setTimeout(() => setShowIntroSplash(false), 5900)
+    const timer = window.setTimeout(() => setShowIntroSplash(false), 7000)
     return () => window.clearTimeout(timer)
   }, [])
 
